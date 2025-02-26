3e3b001aa669d747ba74347eb7fb4ab0f653e8a6
Marcus Ritter <ritter.marcus93@gmail.com>
2025-02-26 22:03:15 +0100

    added code to generatre a larger map

---
+++ generate_map.py
@@ -0,0 +1,22 @@
+
+import json
+
+data = {}
+data["map"] = []
+
+num_rows = 50
+num_cols = 50
+
+for col in range(num_cols):
+    x = []
+    for row in range(num_rows):
+        if col == 0 or col == 1 or row == 0 or row == 1 or col == num_cols-1 or row == num_rows-1 or col == num_cols-2 or row == num_rows-2:
+            x.append(1)
+        else:
+            x.append(2)
+    data["map"].append(x)
+
+print(data)
+
+with open("quadratic_island.json", "w") as file:
+    json.dump(data, file)
